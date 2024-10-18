a0c58d7674cb6c52b528c48ed12ea19a46316d26
youcef <abdelhakim1cef@gmail.comgit config --global user.email abdelhakim1cef@gmail.comgit config --global user.email abdelhakim1cef@gmail.com>
2024-10-18 16:00:14 -0700

    2

---
+++ src/component/Home/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <section className="home-page">
+            <div className="title">
+                <h1><span>Welcom</span> Admin</h1>
+            </div>
+            <h1>coming soon...</h1>
+        </section>
+    );
+}
+export default Home;
